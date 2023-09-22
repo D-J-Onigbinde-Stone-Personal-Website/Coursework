0b3d210a6680e2c4e94ce42a16180f156e8c6237
Doyin Jordan Onigbinde-Stone <126532623+DJStone@users.noreply.github.com>
2023-09-21 22:51:50 -0400

    Add files via upload

--- if_else.js
+++ if_else.js
@@ -1,71 +1,88 @@
-// "I am a string" is true
-// false	is false
-// null is false
-// undefined	is false
-// 0	is false
-// "" is false
+/* 
+"I am a string" is true
+false is false
+null is false
+undefined is false
+0 is false
+"" is false 
+*/
 
-if (input = "I am a string") {
+let input = 
+
+if (input == "I am a string") {
     console.log(true)
-} else if (input = "") {
-    console.log("The empty string is falsy (the only falsy string)")
-} else if (input = 0) {
+} else if (input === "") {
+    console.log("The empty string is falsy (the only falsy string")
+} else if (input === 0) {
     console.log("The number 0 is falsy (the only falsy number)")
-} else if (input = undefined) {
+} else if (input === undefined) {
     console.log("undefined is falsy")
-} else if (input = null) {
+} else if (input === null) {
     console.log("The null value is falsy")
-} else if (input = false) {
+} else if (input === false) {
     console.log("The boolean value false is falsy")
-};
+}
 
-//const sum = x + y;
-// x + y "is less than -1000";
-// x + y "is a negative number";
-// x + y "is equal to 0";
-// x + y "is larger than 0";
-// x + y "is greater than 100"
+/*
+const sum = x + y
+x + y "is less than -1000"
+x + y "is a negative number"
+x + y "is equal to 0"
+x + y "is larger than 0"
+x + y "is greater than 100"
+*/
 
-const x = 50;
-const y = 51;
+let value1 = 
+let value2 =
 
+const sum = value1 + value2
 
 if (sum < -1000) {
-    console.log(sum + "is less than -1000")
+    console.log(sum + " is less than -1000")
 } else if (sum < 0) {
-    console.log(sum + "is a negative number")
-} else if (sum = 0) {
-    console.log(sum + "is equal to 0")
+    console.log(sum + " is a negative number")
+} else if (sum === 0) {
+    console.log(sum + " is equal to 0")
 } else if (sum > 0 && sum < 100) {
-    console.log(sum + "is greater than 0")
+    console.log(sum + " is greater than 0")
 } else if (sum > 100) {
-    console.log(sum + "is greater than 100")
+    console.log(sum + " is greater than 100")
 };
 
-// x & y ≥ 5
-// 5 & 6 are true
-// 10 & 11 are true
-// 0 & 0 are false
-// 1000 & -1000 are false
-// 6 & 4 are false
-// 5 & 5 are true
+/*
+x & y ≥ 5
+5 & 6 are true
+10 & 11 are true
+0 & 0 are false
+1000 & -1000 are false
+6 & 4 are false
+5 & 5 are true
+*/
 
-const x = 5;
-const y = 5;
+let x = 
+let y =
 
-if (x ≥ 5 & y ≥ 5) {
+
+if (x >= 5 && y >= 5) {
     console.log(true)
 } else {
     console.log(false)
 }
 
-// const is true if 
-// x = y or,
-// a = b 
-// otherwise const is false
+/*
+const is true if 
+x = y or,
+a = b 
+otherwise const is false
+*/
 
-if (x = y && a = b) {
-    console.log(true);
+let x = 
+let y = 
+let a = 
+let b = 
+
+if (x === y || a === b) {
+    console.log(true)
 } else {
     console.log(false);
 }
